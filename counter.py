1391e8993ef195a7c52ff572a3fef103078eb9bd
tlneff <neff.tyler@gmail.com>
2024-04-19 22:44:08 -0500

    add program logic

--- counter.py
+++ counter.py
@@ -1,10 +1,24 @@
 def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
-    if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
-        
+    if len(name) < 1:
+        name = "mbox-short.txt"
+    lines = []
+    addresses = {}
+    with open(name) as file:
+        for line in file:
+            lines.append(line)
+    for line in lines:
+        if line[:5] == "From ":
+            address = line.split(' ')[1]
+            addresses[address] = addresses.get(address, 0) + 1
+    maxaddress = ""
+    for address, count in addresses.items():
+        if count > addresses.get(maxaddress, 0):
+            maxaddress = address
+    print(maxaddress, addresses[maxaddress])
 
-## if you want to test locally run > python counter.py
+
+# if you want to test locally run > python counter.py
 if __name__ == "__main__":
     countEmail()
